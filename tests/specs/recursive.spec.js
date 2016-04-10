49d644570c643f303f8e1aeeaa24439e355c3def
Emil Ajdyna <emil@ajdyna.com>
2016-04-10 12:00:52 +0200

    add: more tests for the recursive option

--- tests/specs/recursive.spec.js
+++ tests/specs/recursive.spec.js
@@ -20,7 +20,7 @@ describe("recursive", function () {
 
     });
 
-    it("should be possible to load modules from different folders recursively", function () {
+    it("should be possible to load modules from different folders recursively (example 1)", function () {
     
         var path = __dirname + "/../fixtures/dir_15",
             modules;
@@ -32,6 +32,28 @@ describe("recursive", function () {
 
         expect(modules["en/terms-of-use"]).to.be.defined;
         expect(modules["pl/terms-of-use"]).to.be.defined;
+
+    });
+
+    it("should be possible to load modules from different folders recursively (example 2)", function () {
+
+        var path = __dirname + "/../fixtures/dir_16",
+            modules;
+
+        modules = simpleload(path, {
+            extension: "html",
+            recursive: true
+        });
+
+        expect(modules["en/four-years-old-seedling-40-45cm"]).to.be.defined;
+        expect(modules["en/three-years-old-seedling-30-35cm"]).to.be.defined;
+        expect(modules["en/two-years-old-seedling-20-25cm"]).to.be.defined;
+        expect(modules["en/two-years-old-seedling-10-15cm"]).to.be.defined;
+        expect(modules["pl/sadzonka-czteroletnia-40-45cm"]).to.be.defined;
+        expect(modules["pl/sadzonka-dwuletnia-15-20cm"]).to.be.defined;
+        expect(modules["pl/sadzonka-dwuletnia-25-30cm"]).to.be.defined;
+        expect(modules["pl/sadzonka-roczna-10-15cm"]).to.be.defined;
+        expect(modules["pl/sadzonka-trzyletnia-30-35cm"]).to.be.defined;
 
     });
 
