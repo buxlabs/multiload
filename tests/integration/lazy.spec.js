dfd7a0b5c32321975599c3e0a0eb54d64e97966c
Emil Ajdyna <emil@ajdyna.com>
2016-05-17 19:54:52 +0200

    add: lazy option basics

--- tests/integration/lazy.spec.js
+++ tests/integration/lazy.spec.js
@@ -5,12 +5,26 @@ const assert = require("assert");
 
 describe("lazy", function () {
 
-	it("should be possible to load modules lazily", function () {
+    it("should reserve given namespaces", function () {
 
-		var plugins = simpleload(__dirname, { lazy: true });
+        var plugins = simpleload(__dirname + "/../fixtures/dir_22/package.json", { lazy: true });
 
-		assert(plugins.eslint, "eslint should be available");
+        assert(plugins.chai, "chai should be defined");
+        assert(plugins.eslint, "eslint should be available");
+        assert(plugins.istanbul, "istanbul should be defined");
+        assert(plugins.mocha, "mocha should be defined");
+        assert(plugins.coveralls, "coveralls should be defined");
 
-	});
-	
+    });
+
+    it("should load the module on demand", function () {
+
+        this.timeout(5000);
+
+        var plugins = simpleload(__dirname + "/../fixtures/dir_22/package.json", { lazy: true });
+        var eslint = plugins.eslint();
+        assert(eslint, "eslint should be loaded on demand");
+
+    });
+    
 });
