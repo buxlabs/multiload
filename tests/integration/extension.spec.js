d559df1fa1249d2b647b99f1c95b9b9d92911170
Emil Ajdyna <emil@ajdyna.com>
2016-07-25 22:27:35 +0200

    fix: bug with extension load when a folder is inside the list

--- tests/integration/extension.spec.js
+++ tests/integration/extension.spec.js
@@ -21,6 +21,20 @@ describe("extension", function () {
 
     });
 
+    it("should be possible to load files with extension when a folder exists", function () {
+
+        var modules = simpleload(path.join(__dirname + "/../fixtures/dir_24"), {
+            extension: "html"
+        });
+
+        expect(modules.firstArticle).to.be.a("string");
+        expect(modules.secondArticle).to.be.a("string");
+
+        expect(modules.firstArticle).to.equal("hello world1");
+        expect(modules.secondArticle).to.equal("hello world2");
+
+    });
+
     it("should be possible to load json files", function () {
 
         var modules = simpleload(path.join(__dirname, "/../fixtures/dir_18"), {
