7e3e30cb1ddb7dc4223e65a8dac82e1d512b4f6f
Emil Ajdyna <emil@ajdyna.com>
2016-11-30 21:01:44 +0100

    edit: remove obsolete spec

--- test/spec/lazy.js
+++ test/spec/lazy.js
@@ -7,18 +7,12 @@ test('should reserve namespaces based on dev dependencies', t => {
     t.truthy(plugins.chai, 'chai should be defined');
     t.truthy(plugins.eslint, 'eslint should be available');
     t.truthy(plugins.istanbul, 'istanbul should be defined');
-    t.truthy(plugins.mocha, 'mocha should be defined');
-    t.truthy(plugins.coveralls, 'coveralls should be defined');
+    t.truthy(plugins.mocha, 'ava should be defined');
+    t.truthy(plugins.ava, 'coveralls should be defined');
 });
 
 test('should reserve namespaces based on dependencies', t => {
     var plugins = simpleload(__dirname + '/../fixture/dir_23/package.json', { lazy: true });
 
     t.truthy(plugins.express, 'express should be defined');
-});
-
-test('should load the module on demand', t => {
-    var plugins = simpleload(__dirname + '/../fixture/dir_22/package.json', { lazy: true });
-    var eslint = plugins.eslint();
-    t.truthy(eslint, 'eslint should be loaded on demand');
 });
