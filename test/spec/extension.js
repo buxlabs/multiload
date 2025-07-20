293654e7449225007fa8e67710658f42a2c227f0
Emil Ajdyna <emilajdyna@gmail.com>
2025-07-20 21:47:29 +0200

    Convert tests from ava to node:test

--- test/spec/extension.js
+++ test/spec/extension.js
@@ -1,36 +1,37 @@
-const test = require('ava')
+const { test } = require('node:test')
+const assert = require('node:assert')
 const path = require('path')
 const multiload = require('../..')
 
-test('should be possible to load html files', assert => {
+test('should be possible to load html files', () => {
   const modules = multiload(path.join(__dirname, '../fixture/dir_09'), {
     extension: 'html'
   })
 
-  assert.truthy(typeof modules.firstArticle === 'string')
-  assert.truthy(typeof modules.secondArticle === 'string')
+  assert.ok(typeof modules.firstArticle === 'string')
+  assert.ok(typeof modules.secondArticle === 'string')
 
-  assert.truthy(modules.firstArticle === '<h1>Hello world</h1>')
-  assert.truthy(modules.secondArticle === '<h1>Second article</h1>')
+  assert.strictEqual(modules.firstArticle, '<h1>Hello world</h1>')
+  assert.strictEqual(modules.secondArticle, '<h1>Second article</h1>')
 })
 
-test('should be possible to load files with extension when a folder exists', assert => {
+test('should be possible to load files with extension when a folder exists', () => {
   const modules = multiload(path.join(__dirname, '../fixture/dir_24'), {
     extension: 'html'
   })
 
-  assert.truthy(typeof modules.firstArticle === 'string')
-  assert.truthy(typeof modules.secondArticle === 'string')
+  assert.ok(typeof modules.firstArticle === 'string')
+  assert.ok(typeof modules.secondArticle === 'string')
 
-  assert.truthy(modules.firstArticle === 'hello world1')
-  assert.truthy(modules.secondArticle === 'hello world2')
+  assert.strictEqual(modules.firstArticle, 'hello world1')
+  assert.strictEqual(modules.secondArticle, 'hello world2')
 })
 
-test('should be possible to load json files', assert => {
+test('should be possible to load json files', () => {
   const modules = multiload(path.join(__dirname, '../fixture/dir_18'), {
     extension: 'json'
   })
 
-  assert.deepEqual(JSON.parse(modules.data_01), { hello: 'world' })
-  assert.deepEqual(JSON.parse(modules.data_02), { world: 'hello' })
+  assert.deepStrictEqual(JSON.parse(modules.data_01), { hello: 'world' })
+  assert.deepStrictEqual(JSON.parse(modules.data_02), { world: 'hello' })
 })
--- test/spec/recursive.js
+++ test/spec/recursive.js
@@ -1,30 +1,31 @@
-const test = require('ava')
+const { test } = require('node:test')
+const assert = require('node:assert')
 const path = require('path')
 const multiload = require('../..')
 
-test('should be possible to load modules from different folders recursively (example 1)', assert => {
+test('should be possible to load modules from different folders recursively (example 1)', () => {
   const modules = multiload(path.join(__dirname, '/../fixture/dir_15'), {
     extension: 'html',
     recursive: true
   })
 
-  assert.truthy(modules['en/terms-of-use'])
-  assert.truthy(modules['pl/terms-of-use'])
+  assert.ok(modules['en/terms-of-use'])
+  assert.ok(modules['pl/terms-of-use'])
 })
 
-test('should be possible to load modules from different folders recursively (example 2)', assert => {
+test('should be possible to load modules from different folders recursively (example 2)', () => {
   const modules = multiload(path.join(__dirname, '/../fixture/dir_16'), {
     extension: 'html',
     recursive: true
   })
 
-  assert.truthy(modules['en/four-years-old-seedling-40-45cm'])
-  assert.truthy(modules['en/three-years-old-seedling-30-35cm'])
-  assert.truthy(modules['en/two-years-old-seedling-20-25cm'])
-  assert.truthy(modules['en/two-years-old-seedling-10-15cm'])
-  assert.truthy(modules['pl/sadzonka-czteroletnia-40-45cm'])
-  assert.truthy(modules['pl/sadzonka-dwuletnia-15-20cm'])
-  assert.truthy(modules['pl/sadzonka-dwuletnia-25-30cm'])
-  assert.truthy(modules['pl/sadzonka-roczna-10-15cm'])
-  assert.truthy(modules['pl/sadzonka-trzyletnia-30-35cm'])
+  assert.ok(modules['en/four-years-old-seedling-40-45cm'])
+  assert.ok(modules['en/three-years-old-seedling-30-35cm'])
+  assert.ok(modules['en/two-years-old-seedling-20-25cm'])
+  assert.ok(modules['en/two-years-old-seedling-10-15cm'])
+  assert.ok(modules['pl/sadzonka-czteroletnia-40-45cm'])
+  assert.ok(modules['pl/sadzonka-dwuletnia-15-20cm'])
+  assert.ok(modules['pl/sadzonka-dwuletnia-25-30cm'])
+  assert.ok(modules['pl/sadzonka-roczna-10-15cm'])
+  assert.ok(modules['pl/sadzonka-trzyletnia-30-35cm'])
 })
